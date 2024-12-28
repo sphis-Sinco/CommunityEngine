d6e120b64c1a6754e1e9cee50f6cb7e35fbee3a1
IdealistCat <mattallan.h@gmail.com>
2024-12-28 09:21:10 -0600

    2 variables into 1 thats only allowed in debug

--- source/play/PlayState.hx
+++ source/play/PlayState.hx
@@ -3,26 +3,23 @@ package play;
 
 class PlayState extends MusicBeatState
 {
-	var beat:FlxText = new FlxText(0, 10, 0);
-	var step:FlxText = new FlxText(0, 20, 0);
+	var debugField:FlxText = new FlxText(0, 10, 0);
 
 	override public function create()
 	{
 		FlxG.sound.play(BackendAssets.track('songs/tutorial/Inst'));
 		Conductor.changeBPM(100);
 
-		add(beat);
-		add(step);
+		#if debug add(debugField); #end
 
 		super.create();
 	}
 
 	override public function update(elapsed:Float)
 	{
-		beat.text = "Beat: " + curBeat;
-		step.text = "Step: " + curStep;
-
 		Conductor.songPosition += FlxG.elapsed * 1000;
+		debugField.text = "Beat: " + curBeat;
+		debugField.text += "\nStep: " + curStep;
 
 		super.update(elapsed);
 	}
